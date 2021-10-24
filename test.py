a5dab34909848e90ab5e9869a457d36300b5929f
dariocruz96 <dariocruz96@hotmail.com>
2021-10-24 06:38:21 +0100

    Ended all code for the REST API Added get, put and patch methods.

--- test.py
+++ test.py
@@ -2,12 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [{"likes": 78, "name": "tim", "view": 100000},
-        {"likes": 10000, "name": "tim", "view": 800000},
-        {"likes": 35, "name": "tim", "view": 2000}]
-
-response = requests.put(BASE + "video/3", {"likes": 10, "name": "tim", "view": 100000})
-print(response.json())
-input()
-response = requests.get(BASE + "video/1")
+response = requests.put(BASE + "video/1", {"views": 99, "likes": 100})
 print(response.json())
